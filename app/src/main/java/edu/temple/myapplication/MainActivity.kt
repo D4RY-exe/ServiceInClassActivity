98428b48f4b1960d8408f2e529d005e0829a0c36
D4RY-exe <darynessmc@gmail.com>
2025-04-10 11:48:13 -0400

    attempting to finish, implementing functions

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -3,7 +3,10 @@ package edu.temple.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
+
 //modify ui to trigger things using a menu
 //take callbacks into a function and call function?
 
@@ -14,16 +17,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
-
+onStartClicked()
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+onStopClicked()
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_start -> { onStartClicked() }
+            R.id.action_stop -> { onStopClicked() }
+            else -> return false
+    }
+        return true
+    }
+    private fun onStartClicked() {
+        Toast.makeText(this, "Start clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onStopClicked() {
+        Toast.makeText(this, "Stop clicked", Toast.LENGTH_SHORT).show()
     }
 }
