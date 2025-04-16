30178a58cd4547ab5a4fa30ad4d24ddae126bf3c
D4RY-exe <darynessmc@gmail.com>
2025-04-16 15:19:06 -0400

    modifiying onstart and onstop

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,5 +1,7 @@
 package edu.temple.myapplication
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -9,8 +11,13 @@ import android.widget.Toast
 
 //modify ui to trigger things using a menu
 //take callbacks into a function and call function?
+//make start and stop buttons on ui
 
 class MainActivity : AppCompatActivity() {
+
+    private val PREFS_NAME = "countdown_prefs"
+    private val KEY_SAVED_COUNT = "saved_count"
+    private val DEFAULT_COUNT = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,10 +46,21 @@ onStopClicked()
         return true
     }
     private fun onStartClicked() {
+        val prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
+        val savedCount = prefs.getInt(KEY_SAVED_COUNT, -1)
+        val startValue = if (savedCount > 0) savedCount else DEFAULT_COUNT
+
+        val serviceIntent = Intent(this, TimerService::class.java)
+        bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE)
+
+        timerBinder?.start(startValue)
         Toast.makeText(this, "Start clicked", Toast.LENGTH_SHORT).show()
     }
 
     private fun onStopClicked() {
+        timerBinder?.pause()
+        val currentValue = currentCount
+        getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit().putInt(KEY_SAVED_COUNT, currentValue).apply()
         Toast.makeText(this, "Stop clicked", Toast.LENGTH_SHORT).show()
     }
 }
